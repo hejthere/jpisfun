2fe5f930ca2c3030f3992f42fc4e4eda21eb4a89
hejthere <69143702+hejthere@users.noreply.github.com>
2020-12-19 20:49:22 +0800

    Route Attempt 8

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Grammar from './component/Grammar/Grammar';
 import Contact from './component/Contact/Contact'
 import Banner from './component/Home/Banner'
 import VocabDiseases from './component/Home/VocabDiseases';
-import OtherResources from './Resources/OtherResources';
+import OtherResources from './component/Resources/OtherResources';
 import RandomQuote from './component/Vocaburary/RandomQuote';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faIgloo, faVial, faAddressBook, faPencilAlt, faGlasses } from '@fortawesome/free-solid-svg-icons'; 
@@ -20,9 +20,22 @@ import miseimage from '../src/Picture/mise.jpg';
 import Katakana from './component/Home/Katakana';
 
 class App extends Component {
-  state= {
-  pathname: window.location.pathname,
-  }
+
+  NavButtonHandler =()=> { 
+      var navBar =  document.getElementById('navBar');
+      if (navBar.style.display === "block"){ 
+        navBar.style.display = "none";
+      };
+      if (navBar.style.display === "none"){ 
+        navBar.style.display = "block";
+      };
+    }
+  
+    CollapseHandler =()=> { 
+      var navBar =  document.getElementById('navBar'); 
+      navBar.style.display = "none";
+      }
+  
 
 
   render () {
@@ -32,21 +45,19 @@ class App extends Component {
    <div>
 
    <Container fluid>
-   <Navbar collapseOnSelect expand='md' bg="dark" variant="dark">
-   <Navbar.Toggle  />
-   <Navbar.Collapse id="responsive-navbar-nav">
-     <Nav className='navBar' fill fluid>
-		<Nav.Link as="a" href='/'><FontAwesomeIcon icon={faIgloo} /> Home</Nav.Link> 
-		<Nav.Link as="a" href='/test'><FontAwesomeIcon icon={faVial} /> Test Your Knowledge</Nav.Link>
-    <Nav.Link as="a" href='/grammar'><FontAwesomeIcon icon={faPencilAlt} /> Grammar Practice</Nav.Link>
-		<Nav.Link as="a" href='/resources'><FontAwesomeIcon icon={faGlasses} /> Resources</Nav.Link> 
-    <Nav.Link as="a" href='/contactus'><FontAwesomeIcon icon={faAddressBook} /> Contact Us</Nav.Link>
-     </Nav>
-     </Navbar.Collapse>
+   <Navbar expand='md' bg="dark" variant="dark">
+   <Navbar.Toggle onClick={this.NavButtonHandler} />
+   <Navbar.Collapse >
+    <Nav  id='navBar' className='navBar' fill fluid>
+    <Link onClick={this.CollapseHandler} className='nav-link' to='/'><FontAwesomeIcon icon={faIgloo} /> Home</Link> 
+		<Link onClick={this.CollapseHandler} className='nav-link' to='/test'><FontAwesomeIcon icon={faVial} /> Test Your Knowledge</Link>
+    <Link onClick={this.CollapseHandler} className='nav-link' to='/grammar'><FontAwesomeIcon icon={faPencilAlt} /> Grammar Practice</Link>
+		<Link onClick={this.CollapseHandler} className='nav-link' to='/resources'><FontAwesomeIcon icon={faGlasses} /> Resources</Link> 
+    <Link onClick={this.CollapseHandler} className='nav-link' to='/contactus'><FontAwesomeIcon icon={faAddressBook} /> Contact Us</Link>
+    </Nav>
+    </Navbar.Collapse>
     </Navbar>
     </Container>
-
-  
 
 
   <Switch>
