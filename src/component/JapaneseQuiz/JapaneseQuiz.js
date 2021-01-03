1b05c3574c704f3ea71a83083ea8aa2af2a39582
hejthere <69143702+hejthere@users.noreply.github.com>
2021-01-04 00:11:41 +0800

    flipcard7

--- src/component/JapaneseQuiz/JapaneseQuiz.js
+++ src/component/JapaneseQuiz/JapaneseQuiz.js
@@ -1,31 +1,32 @@
 import React from 'react';
-import {Container} from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import Grammar from './Grammar/Grammar';
 import Katakana from './katakana/Katakana';
 import Test from './Test/Test';
 import './JapaneseQuiz.css';
 
-const JapaneseQuiz =()=> {
+const JapaneseQuiz = () => {
 
-return(
-<Container fluid> 
-<section id="katakana">
-<Katakana />
-<br />
-<br />
-</section>
-<section id="test">
-<Test />
-<br />
-<br />  
-</section>
-<section id="grammar">
-<Grammar />
-<br />
-<br />
-</section>
-</Container>
+    return (
+        <Container fluid>
+            <section id="katakana">
+                <Katakana />
+                <br />
+                <br />
+            </section>
+            <section id="test">
+                <Test />
+                <br />
+                <br />
+            </section>
+            <section id="grammar">
+                <Grammar />
+                <br />
+                <br />
+            </section>
+        </Container>
 
-)}
+    )
+}
 
 export default JapaneseQuiz;
--- src/component/JapaneseQuiz/katakana/Katakana.js
+++ src/component/JapaneseQuiz/katakana/Katakana.js
@@ -3,71 +3,75 @@ import './katakana.css';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 
-class Katakana extends Component { 
+class Katakana extends Component {
 
-state = { 
-question: [ 
-{id:1, katakana:'あ', option:['a' ,'i','u','e','o'], answer:'a'},
-{id:2, katakana:'い', option:['a' ,'i','u','e','o'], answer:'i'},
-{id:3, katakana:'う', option:['a' ,'i','u','e','o'], answer:'u'},
-{id:4, katakana:'え', option:['a' ,'i','u','e','o'], answer:'u'},
-{id:5, katakana:'お', option:['a' ,'i','u','e','o'], answer:'o'},
-{id:6, katakana:'か', option:['ka','ki','ku','ke','ko'], answer:'ka'},
-{id:7, katakana:'き', option:['ka','ki','ku','ke','ko'], answer:'ki'},],
-displayQuestion: 'あ',
-displayOption: ['a' ,'i','e','o'],
-displayAnswer: 'a',
+    state = {
+        question: [
+            { id: 1, katakana: 'あ', option: ['a', 'i', 'u', 'e', 'o'], answer: 'a' },
+            { id: 2, katakana: 'い', option: ['a', 'i', 'u', 'e', 'o'], answer: 'i' },
+            { id: 3, katakana: 'う', option: ['a', 'i', 'u', 'e', 'o'], answer: 'u' },
+            { id: 4, katakana: 'え', option: ['a', 'i', 'u', 'e', 'o'], answer: 'u' },
+            { id: 5, katakana: 'お', option: ['a', 'i', 'u', 'e', 'o'], answer: 'o' },
+            { id: 6, katakana: 'か', option: ['ka', 'ki', 'ku', 'ke', 'ko'], answer: 'ka' },
+            { id: 7, katakana: 'き', option: ['ka', 'ki', 'ku', 'ke', 'ko'], answer: 'ki' },],
+        displayQuestion: 'あ',
+        displayOption: ['a', 'i', 'e', 'o'],
+        displayAnswer: 'a',
+    }
+
+
+    answerHandler = (e) => {
+        if (e.target.innerHTML === this.state.displayAnswer) {
+            alert('correct!');
+            let randomNumber = Math.floor(Math.random() * 3);
+            this.setState({
+                displayQuestion: this.state.question[randomNumber].katakana,
+                displayOption: this.state.question[randomNumber].option,
+                displayAnswer: this.state.question[randomNumber].answer
+            })
+            let button = document.getElementsByClassName('katakana-button');
+            var i;
+            for (i = 0; i < button.length; i++) {
+                button[i].style.border = '2px solid var(--blue)';
+                button[i].style.color = 'white';
+
+            }
+        } else {
+            e.target.style.color = 'red'
+            e.target.style.border = '2px solid red'
+        }
+    }
+
+    render() {
+
+        let options = '';
+        options = this.state.displayOption.map(option => {
+            return (
+                <Button
+                    className='katakana-button'
+                    onClick={this.answerHandler}
+                    key={option.id}>
+                    {option}</Button>
+            )
+        }
+        )
+
+
+        return (
+            <Container fluid>
+                <h5 className='katakana-introduction'>
+                    Level 1: Do you know the romaji of the following katakana ? </h5>
+
+                <div className='katakana-container'>
+                    <br />
+                    <p className='katakana-question'> {this.state.displayQuestion}</p>
+                </div>
+                {options}
+                <br />
+                <br />
+            </Container>
+        )
+    }
 }
-
-
-answerHandler =(e)=> {
- if(e.target.innerHTML === this.state.displayAnswer){ 
-alert('correct!');
-let randomNumber = Math.floor(Math.random()*3);
-this.setState({
-displayQuestion: this.state.question[randomNumber].katakana, 
-displayOption: this.state.question[randomNumber].option,
-displayAnswer: this.state.question[randomNumber].answer
-})
-let button = document.getElementsByClassName('katakana-button');
-var i;
-for (i = 0; i < button.length; i++) {
-    button[i].style.border='2px solid var(--blue)';
-    button[i].style.color='white';
-
-}
-}else{ 
-e.target.style.color='red'
-e.target.style.border='2px solid red'
-}}
-
-render() {
-
-   let options =''; 
-   options=  this.state.displayOption.map( option => {
-    return ( 
-        <Button
-        className='katakana-button'
-        onClick={this.answerHandler}
-        key={option.id}>
-        {option}</Button>
-    )}
-    )
-
-
-return ( 
-<Container fluid>
-<h5 className='katakana-introduction'>
-   Level 1: Do you know the romaji of the following katakana ? </h5>
-
-<div className='katakana-container'> 
-<br />
-<p className='katakana-question'> {this.state.displayQuestion}</p>
-</div>
-{options}
-<br />
-<br />
-</Container>
-)}}
 
 export default Katakana;
