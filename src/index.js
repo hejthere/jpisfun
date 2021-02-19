1f2d203b337af9e7c8672978444b9461ed954e12
hejthere <69143702+hejthere@users.noreply.github.com>
2021-02-19 10:40:22 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 
-ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
