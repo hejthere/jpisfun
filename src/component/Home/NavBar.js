8bca155223f0de8bf05bfe701068d90bf50b43a7
hejthere <69143702+hejthere@users.noreply.github.com>
2021-02-20 12:36:57 +0800

    drycode9

--- src/component/Home/NavBar.js
+++ src/component/Home/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../../App.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faIgloo, faVial, faAddressBook, faPencilAlt, faGlasses } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { faIgloo, faVial, faAddressBook, faPencilAlt, faGlasses } from '@fortawesome/free-solid-svg-icons';
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 
@@ -15,29 +15,29 @@ export default function NavBar() {
                 <Navbar.Toggle />
                 <Navbar.Collapse>
                     <Nav id='navBar1' className='navBar' fill fluid="true">
-                        <Nav.Link className='nav-link' href='/'>
-                            <FontAwesomeIcon icon={faIgloo} />
-                             Home</Nav.Link>
+                        <Link className='nav-link' to='/'>
+                            {/* <FontAwesomeIcon icon={faIgloo} /> */}
+                             Home</Link>
                         <NavDropdown title={<span>
-                            <FontAwesomeIcon icon={faVial} />
+                            {/* <FontAwesomeIcon icon={faVial} />  */}
                             Quiz</span>} id="nav-dropdown">
                             <NavDropdown.Item eventKey="1.1"><Link to="/quiz/katakana">(Level 1) Katakana Test</Link></NavDropdown.Item>
                             <NavDropdown.Item eventKey="1.2"><Link to="/quiz/test">(Level 2) Greetings</Link></NavDropdown.Item>
                             <NavDropdown.Item eventKey="1.3"><Link to="/quiz/grammar">(Level 3) Grammar Practice</Link></NavDropdown.Item>
                         </NavDropdown>
                         <NavDropdown title={<span>
-                            <FontAwesomeIcon icon={faPencilAlt} />
+                            {/* <FontAwesomeIcon icon={faPencilAlt} />  */}
                             Vocabulary</span>} id="nav-dropdown">
                             <NavDropdown.Item eventKey="2.1"><Link to='/vocabulary/kanji'>Kanji</Link></NavDropdown.Item>
                             <NavDropdown.Item eventKey="2.2"><Link to='/vocabulary/quotes'>Quotes of Today</Link></NavDropdown.Item>
                             <NavDropdown.Item eventKey="2.3"><Link to="/vocabulary/vocabdiseases">Diseases</Link></NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link className='nav-link' href='/resources'>
-                            <FontAwesomeIcon icon={faGlasses} />
-                            Resources</Nav.Link>
-                        <Nav.Link className='nav-link' href='/contactus'>
-                            <FontAwesomeIcon icon={faAddressBook} />
-                            Contact Us</Nav.Link>
+                        <Link className='nav-link' to='/resources'>
+                            {/* <FontAwesomeIcon icon={faGlasses} />  */}
+                            Resources</Link>
+                        <Link className='nav-link' to='/contactus'>
+                            {/* <FontAwesomeIcon icon={faAddressBook} />  */}
+                            Contact Us</Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
